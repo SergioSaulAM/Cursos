bda6d5dec31394cab46b12aa2e9f47f706da3ed7
SergioSaulAM <ssaulam34@gmail.com>
2023-06-23 12:15:36 -0600

    Agregando contenido de Clases CSS en JavaScript

--- Curso de Javascript/Tipos de Datos/js/dom.js
+++ Curso de Javascript/Tipos de Datos/js/dom.js
@@ -93,7 +93,7 @@ console.log($linkDOM.hasAttribute("data-id"));
 
 
 // ========================= DOM: ESTILOS Y VARIABLES CSS ===========================
-
+/* 
 const $linkDOM = document.querySelector(".link-dom");
 console.log($linkDOM.style);
 console.log($linkDOM.style.backgroundColor);
@@ -125,3 +125,26 @@ $body.style.setProperty("color", varYellowColor);
 $html.style.setProperty("--dark-color", "blue");
 varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 $body.style.setProperty("background-color", varDarkColor);
+*/
+
+
+// ========================= DOM: CLASES CSS ===========================
+
+$card = document.querySelector(".card");
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains("rotate-45"));
+$card.classList.add("rotate-45");
+console.log($card.className);
+console.log($card.classList);
+$card.classList.remove("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+console.log($card.classList.contains("rotate-45"));
+$card.classList.toggle("rotate-45");
+$card.classList.replace("rotate-45", "rotate-135");
+
+$card.classList.add("opacity-80", "sepia");
