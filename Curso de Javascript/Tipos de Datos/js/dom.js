4e79aff7a96b59d56308f8bca2d726b294b4034c
SergioSaulAM <ssaulam34@gmail.com>
2023-06-27 21:45:46 -0600

    Agregando contenido de DOM: Creando Elementos y Fragmentos en JavaScript

--- Curso de Javascript/Tipos de Datos/js/dom.js
+++ Curso de Javascript/Tipos de Datos/js/dom.js
@@ -175,6 +175,7 @@ $whatIsDOM.outerHTML = text;
 
 // ========================= DOM: TRAVERSING: RECORRIENDO EL DOM ===========================
 
+/* 
 const $cards = document.querySelector(".cards");
 
 console.log($cards);
@@ -194,3 +195,86 @@ console.log($cards.nextElementSibling);
 console.log($cards.closest("div"));
 console.log($cards.closest("body"));
 console.log($cards.children[3].closest("section"));
+*/
+
+
+
+// ========================= DOM: CREANDO ELEMENTOS Y FRAGMENTOS ===========================
+
+const $figure = document.createElement("figure"),
+$img = document.createElement("img"),
+$figcaption = document.createElement("figcaption"),
+$figcaptionText = document.createTextNode("Animals"),
+$cards = document.querySelector(".cards"),
+$figure2 = document.createElement("figure");
+
+$img.setAttribute("src", "https://loremflickr.com/200/200/animals");
+$img.setAttribute("alt", "Animals");
+$figure.classList.add("card");
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `
+    <img src="https://loremflickr.com/200/200/people" alt="People">
+    <figcaption>People</figcaption>
+`;
+$figure2.classList.add("card");
+$cards.appendChild($figure2);
+
+// Renderizado de elementos
+
+
+const estaciones = ["Primavera", "Verano", "Otoño", "Invierno"], $ul = document.createElement("ul");
+
+document.write("<h3>Estaciones del Año</h3>");
+
+document.body.appendChild($ul);
+estaciones.forEach(el => {
+    const $li = document.createElement("li");
+    $li.textContent = el;
+    $ul.appendChild($li); 
+});
+
+
+const continentes = ["África", "América", "Asia", "Europa", "Oceanía"],
+$ul2 = document.createElement("ul");
+
+document.write("<h3>Continentes del mundo</h3>");
+
+$ul2.innerHTML = "";
+document.body.appendChild($ul2);
+continentes.forEach(e => ($ul2.innerHTML += `<li>${e}</li>`));
+
+
+
+// Fragmentos
+
+const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+],
+$ul3 = document.createElement("ul"),
+$fragment = document.createDocumentFragment();
+
+meses.forEach(e => {
+    const $li = document.createElement("li");
+    $li.textContent = e;
+    $fragment.appendChild($li);
+});
+
+document.write("<h3>Meses del Año</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3);
