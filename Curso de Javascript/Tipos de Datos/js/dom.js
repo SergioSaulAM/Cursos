ee541f9c01b1f9f527a0f25001f8a523731b405f
SergioSaulAM <ssaulam34@gmail.com>
2024-01-24 14:41:15 -0600

    Agregando contenido de JS DOM: BOM: Objetos URL, Historial y Navegador

--- Curso de Javascript/Tipos de Datos/js/dom.js
+++ Curso de Javascript/Tipos de Datos/js/dom.js
@@ -548,3 +548,37 @@ $btnImprimir.addEventListener('click', (e) => {
 
 
 
+// ========================= BOM: OBJETOS: URL, HISTORIAL Y NAVEGADOR ===========================
+
+console.log('***************** OBJETO URL (location) *****************');
+console.log(location);
+console.log(location.origin);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.port);
+console.log(location.href);
+console.log(location.hash);
+console.log(location.pathname);
+// location.reload()
+
+
+console.log('***************** OBJETO HISTORIAL (history) *****************');
+console.log(history);
+console.log(history.length);
+// history.forward(1);
+// history.go(-3);
+// history.back(2);
+
+
+console.log('***************** OBJETO NAVEGADOR (navigator) *****************');
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
+
